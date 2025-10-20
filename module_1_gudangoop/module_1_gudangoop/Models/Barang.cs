94221aa1c51f94ce4824f6c804ba06292ea90d51
Muhammad Arif <m.arifrivaldi141105@gmail.com>
2025-10-20 10:27:06 +0700

    Add files via upload

---
+++ module_1_gudangoop/module_1_gudangoop/Models/Barang.cs
@@ -0,0 +1,32 @@
+﻿namespace Modul1_GudangOOP.Models;
+public class Barang
+{
+    public string KodeBarang;
+    public string NamaBarang;
+    public int JumlahStok;
+    public string Kategori;
+    public Barang(string kode, string nama, int stok, string kategori)
+    {
+        KodeBarang = kode;
+        NamaBarang = nama;
+        JumlahStok = stok;
+        Kategori = kategori;
+    }
+
+    public void TampilkanInfo()
+    {
+        Console.WriteLine($"Kode Barang: {KodeBarang}");
+        Console.WriteLine($"Nama Barang: {NamaBarang}");
+        Console.WriteLine($"Jumlah Stok: {JumlahStok}");
+        Console.WriteLine($"Kategori: {Kategori}");
+    }
+
+    public Barang() {
+        KodeBarang = "UNKNOWN";
+        NamaBarang = "UNKNOW";
+        JumlahStok = 0;
+        Kategori = "Umum"; 
+    }
+
+}
+
---
+++ module_1_gudangoop/module_1_gudangoop/Program.cs
@@ -0,0 +1,33 @@
+﻿using Modul1_GudangOOP.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Barang barang1 = new Barang("B001", "Laptop", 10, "Elektronik");
+        Barang barang2 = new Barang("B002", "Kursi", 25, "Furnitur");
+        Console.WriteLine("Informasi Barang 1:");
+        barang1.TampilkanInfo();
+        Console.WriteLine("\nInformasi Barang 2:");
+        barang2.TampilkanInfo();
+
+        Barang b3 = new Barang();
+        Console.Write("\nMasukan Nama Barang:");
+        b3.NamaBarang = Console.ReadLine();
+
+        Console.Write("Masukan Kode Barang:");
+        b3.KodeBarang = Console.ReadLine();
+
+        Console.Write("Masukan Jumlah Stok:");
+        b3.JumlahStok = int.Parse(Console.ReadLine() ?? "0");
+
+        Console.Write("Masukan Kategori:");
+        b3.Kategori = Console.ReadLine() ?? "Umum";
+
+        Console.WriteLine("\nInformasi Barang 3:");
+        Console.WriteLine("======================");
+        b3.TampilkanInfo();
+    }
+
+
+}
